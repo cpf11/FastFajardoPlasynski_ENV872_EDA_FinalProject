8ae85cea3554aee156fe135dde17f4f4b246dfb8
Clara Fast <clara@Claras-MBP.lan>
2022-04-10 14:19:14 -0400

    Group work april 10

--- Project Documents/Data Wrangling.R
+++ Project Documents/Data Wrangling.R
@@ -3,7 +3,7 @@ require(lubridate)
 require(tidyverse)
 
 # Load your datasets
-lamb_ewe <- read_csv("../Data/Raw/lamb_ewe_ratios_by_mountainunit.csv")
+lamb_ewe <- read.csv("./Data/Raw/lamb_ewe_ratios_by_mountainunit.csv")
 
 #Change year to date class
 lamb_ewe$year<-as.factor(lamb_ewe$year)
@@ -12,7 +12,13 @@ lamb_ewe$year<-as.factor(lamb_ewe$year)
 lamb_ewe_wrangle<- lamb_ewe %>%
   drop_na (snow_line_elevation) %>%
   select(mountain_unit, year, lamb_ewe_ratio, elevation, snow_line_elevation, 
-         snow_disappearance, snow_cover_duration, temp, precip, may_temp, winter_precip) 
+         snow_disappearance, snow_cover_duration, temp, precip, 
+         may_temp, winter_precip) 
+
+lamb_ewe_wrangle
+
+write.csv(lamb_ewe_wrangle, row.names = FALSE,
+          file = "./Data/Processed/lamb_ewe_wrangle.csv")
 
 #Plot dataset
 plot(lamb_ewe_wrangle)
