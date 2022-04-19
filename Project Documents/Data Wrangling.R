e47193b756e0f5eb2d342463c308a8327343e451
Clara Fast <clara@Claras-MBP.lan>
2022-04-19 14:23:53 -0400

    Final edits

--- Project Documents/Data Wrangling.R
+++ Project Documents/Data Wrangling.R
@@ -20,69 +20,10 @@ lamb_ewe_wrangle
 write.csv(lamb_ewe_wrangle, row.names = FALSE,
           file = "./Data/Processed/lamb_ewe_wrangle.csv")
 
-#Plot dataset
-plot(lamb_ewe_wrangle)
-
-#Models
-
-lamb_lm <- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + elevation + snow_line_elevation + 
-              snow_disappearance + snow_cover_duration + temp + precip + may_temp + winter_precip)
-
-summary(lamb_lm)
-#Choose model by AIC in a Stepwise Algorithm
-step(lamb_lm)
 
 
-new_lamb_lm<-lm(formula = lamb_ewe_ratio ~ year + precip, data = lamb_ewe_wrangle)
-
-summary(new_lamb_lm)
-
-#Backwards stepwise regression
-
-#Take out snow cover duration
-lamb_lm1<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + elevation + snow_line_elevation + 
-                snow_disappearance + temp + precip + may_temp + winter_precip)
-
-summary(lamb_lm1)
-
-#Take out elevation
-lamb_lm2<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + snow_line_elevation + 
-                snow_disappearance + temp + precip + may_temp + winter_precip)
-
-summary(lamb_lm2)
-
-#Take out snow line elevation
-lamb_lm3<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + 
-                snow_disappearance + temp + precip + may_temp + winter_precip)
-
-summary(lamb_lm3)
-
-#Take out snow disappearance
-lamb_lm4<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + temp + precip + may_temp + winter_precip)
-
-summary(lamb_lm4)
-
-#Take out may temperature
-lamb_lm5<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + temp + precip + winter_precip)
-
-summary(lamb_lm5)
-
-#Take out temperature
-lamb_lm6<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + precip + winter_precip)
-
-summary(lamb_lm6)
-
-#Take out winter precipitation
-lamb_lm7<- lm(data = lamb_ewe_wrangle, lamb_ewe_ratio ~ year + precip)
-
-summary(lamb_lm7)
-
-AIC(lamb_lm, lamb_lm7)
 
 
-#Scatterplots, lamb_ewe versus precipitation
-
-#Scatterplots, lamb_ewe versus year
 
 
 
